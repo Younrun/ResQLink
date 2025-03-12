9fd5e01eee0016ce459bf67d87328690c39c7070
Younes Bazzaoui <younrun.rocka@gmail.com>
2025-03-12 00:26:29 +0000

    commit

---
+++ resqlink/accounts/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission
+
+class IsHospitalUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.user_type == 'hospital'
